aae6d716d79c4a0692570deaca8190022114443b
venkibale <34294679+venkibale@users.noreply.github.com>
2020-10-11 18:37:24 +0530

    Create docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Set database config from Heroku DATABASE_URL
+if [ "$DATABASE_URL" != "" ]; then
+    echo "Found database configuration in DATABASE_URL=$DATABASE_URL"
+
+    regex='^postgres://([a-zA-Z0-9_-]+):([a-zA-Z0-9]+)@([a-z0-9.-]+):([[:digit:]]+)/([a-zA-Z0-9_-]+)$'
+    if [[ $DATABASE_URL =~ $regex ]]; then
+        export DB_HOST=${BASH_REMATCH[3]}
+        export DB_PORT=${BASH_REMATCH[4]}
+        export DB_DATABASE=${BASH_REMATCH[5]}
+        export DB_USER=${BASH_REMATCH[1]}
+        export DB_PASSWORD=${BASH_REMATCH[2]}
+    fi
+
+fi
+
+export DB_ADAPTER=postgres
+export DB_PG_SCHEMA=konga
+export DB_SSL=true
